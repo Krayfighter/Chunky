3cd5d639ee9428e62a4ceb15dfbfe65c4c98169b
Aiden Kring <aidenjkring@gmail.com>
2021-06-01 10:30:02 -0400

    add run.ps1 and add Chunker class to main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
 
+class Chunker {
+	vector<string> data;
+	public:
+		Chunker(string input, int chunksize = 4, char filler = '$') {
+			for(int i = 0; i < input.size()%chunksize; i++) {input += filler;}
+			for(int i = 0; i < input.size()/chunksize; i++) {
+				data.push_back("");
+				for(int o = 0; o < chunksize; o++) {data[i] += input[i*chunksize+o];}}
+		}
+		void print() {for(string chunk: data) {cout << chunk << endl;}}
+};
+
 int main() {
-    cout << "Hello, World!" << endl;
+	Chunker text = Chunker("hello", 2);
+	text.print();
 }
---
+++ run.ps1
@@ -0,0 +1,2 @@
+../zig/zig.exe c++ ./main.cpp -o main.exe
+./main.exe
